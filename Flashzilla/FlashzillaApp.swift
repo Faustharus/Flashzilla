12056b12eaa4a2be80c9de92ad7c63a8e9a6ddc7
Faustharus <chailloleau-damien@outlook.fr>
2024-07-27 14:03:49 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Damien Chailloleau on 27/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
