78959d31079b74e5d54b962c36bba04399028985
Faustharus <chailloleau-damien@outlook.fr>
2024-07-30 18:54:59 +0200

    Card swipe available with accessibility

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+    
     @State private var cards = Array<Card>(repeating: .example, count: 10)
+    @State private var isActive: Bool = true
+    @State private var timeRemaining: Int = 100
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         ZStack {
@@ -17,12 +24,61 @@ struct ContentView: View {
                 .resizable()
                 .ignoresSafeArea()
             VStack {
+                Text("\(timeRemaining) seconds left")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 3)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
                 ZStack {
-                    ForEach(0..<cards.count, id: \.self) { item in
-                        CardView(card: cards[item])
-                            .stacked(at: item, in: cards.count)
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        let _ = print("\(cards.count)")
+                        CardView(card: cards[index]) {
+                            removeCard(at: index)
+                        }
+                        .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
                     }
                 }
+            }
+            
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding(5)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "checkmark.circle")
+                            .padding(5)
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
@@ -30,4 +86,13 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+// MARK: Functions & Computed Properpeties
+extension ContentView {
+    
+    func removeCard(at position: Int) {
+        cards.remove(at: position)
+    }
+    
 }
